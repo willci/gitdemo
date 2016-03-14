e53192c2a6cb2841e67e19690393dde600a19d1a
xwl <xwl@xwl-mac-mini.local>
2016-03-14 16:23:28 +0800

    Initial Commit

---
+++ gitdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitdemo
+//
+//  Created by xwl on 16/3/14.
+//  Copyright © 2016年 ci123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitdemo
+//
+//  Created by xwl on 16/3/14.
+//  Copyright © 2016年 ci123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
